c14c331fb0623b0d0e1f3c7c672a3aeb7b601fd9
Krisztián Szűcs <szucs.krisztian@gmail.com>
2014-03-11 12:11:56 +0100

    Fixed libmpi unix extension

--- pyspejd/__init__.pyx
+++ pyspejd/__init__.pyx
@@ -2,7 +2,7 @@ from ctypes import *
 cimport pyspejd.libspejd as libspejd
 include "spejd.pyx"
 
-mpi = CDLL('libmpi.0.dylib', RTLD_GLOBAL)
+mpi = CDLL('libmpi.so', RTLD_GLOBAL)
 f = pythonapi.Py_GetArgcArgv
 argc = c_int()
 argv = POINTER(c_char_p)()
